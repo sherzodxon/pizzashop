0ed3f2f70478c5c8e2b592621c3004f1acacf441
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2025-08-01 10:13:58 +0500

    hamburger gradient is created

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".header__nav-wrapper");
+const gradient = document.querySelector(".header__gradient");
 
 hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("hamburger--close");
     nav.classList.toggle("header__nav-wrapper--open");
+    gradient.classList.toggle("header__gradient--active")
 })
 
+gradient.addEventListener('click',function(){
+    hamburger.classList.remove("hamburger--close");
+    nav.classList.remove("header__nav-wrapper--open");
+    gradient.classList.remove("header__gradient--active")
+})
 const btn1 = document.getElementById("button1");
 const btn2 = document.getElementById("button2");
 
